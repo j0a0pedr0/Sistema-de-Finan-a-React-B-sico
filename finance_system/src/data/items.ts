9597c064e77a263f0cb292379ab1567d4e8a70a3
joao pedro <joaopedrobarreto1000@gmail.com>
2022-05-21 14:51:16 -0300

    inicio da base de dados item e category

---
+++ finance_system/src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2022, 6 , 20), category: 'food' , title: 'McDonalds', value:32.12},
+    { date: new Date(2022, 6 , 20), category: 'food' , title: 'Bruger King', value:25},
+    { date: new Date(2022, 6 , 20), category: 'rent' , title: 'Aluguel apt', value:32.12},
+    { date: new Date(2022, 6 , 20), category: 'food' , title: 'frangos', value:32.12},
+    { date: new Date(2022, 6 , 20), category: 'salary' , title: 'Salario trade', value:30000}
+];
