cfdc9b66f90338f61f8e3647a90d70693e6221df
joao pedro <joaopedrobarreto1000@gmail.com>
2022-05-24 04:47:43 -0300

    Finalização do projeto

--- finance_system/src/components/InfoArea/index.tsx
+++ finance_system/src/components/InfoArea/index.tsx
@@ -2,21 +2,48 @@ import * as C from './styles';
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { formatCurrentMonth } from '../../helpers/dateFilter'
+import { ResumeItem} from '../ResumeItem'
 
 type Props = {
     currentMonth: string;
+    onMonthChange: ( newMonth: string) => void;
+    income: number;
+    expense:number;
 }
 
-export const InfoArea = ({ currentMonth }: Props) => {
+export const InfoArea = ({ currentMonth, onMonthChange, income, expense}: Props) => {
+
+    const handlePrevMonth = () => {
+        let [year, month] = currentMonth.split('-');
+        let currentDate = new Date(parseInt(year), parseInt(month) - 1,1) 
+        currentDate.setMonth( currentDate.getMonth() - 1);
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`)
+    }
+
+    const handleNextMonth = () => {
+        let [year, month] = currentMonth.split('-');
+        let currentDate = new Date(parseInt(year), parseInt(month) - 1,1) 
+        currentDate.setMonth( currentDate.getMonth() + 1);
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`)
+    }
+
+
+
     return (
         <C.Container>
             <C.MonthArea>
-                <C.MonthArrow>< BsFillArrowLeftCircleFill size={30}></BsFillArrowLeftCircleFill></C.MonthArrow>
+                <C.MonthArrow>< BsFillArrowLeftCircleFill onClick={handlePrevMonth} size={30}></BsFillArrowLeftCircleFill></C.MonthArrow>
                 <C.MonthTitle>{formatCurrentMonth(currentMonth)}</C.MonthTitle>
-                <C.MonthArrow><BsFillArrowRightCircleFill size={30}></BsFillArrowRightCircleFill></C.MonthArrow>
+                <C.MonthArrow><BsFillArrowRightCircleFill onClick={handleNextMonth} size={30}></BsFillArrowRightCircleFill></C.MonthArrow>
             </C.MonthArea>
             <C.ResumeArea>
-
+                <ResumeItem title='Receitas' value={income}/>
+                <ResumeItem title='Despesas' value={expense}/>
+                <ResumeItem 
+                    title='Balanço' 
+                    value={income - expense}
+                    color={(income - expense) < 0 ? 'red' : 'green'}
+                    />
             </C.ResumeArea>
         </C.Container>
     );
--- finance_system/src/data/items.ts
+++ finance_system/src/data/items.ts
@@ -3,8 +3,20 @@ import { Item } from '../types/Item'
 export const items: Item[] = [
     { date: new Date(2022, 4 , 7), category: 'food' , title: 'Mc Donalds', value:32.12},
     { date: new Date(2022, 4 , 21), category: 'food' , title: 'Bruger King', value:25},
-    { date: new Date(2022, 4 , 21), category: 'rent' , title: 'Aluguel apt', value:3002.12},
+    { date: new Date(2022, 4 , 21), category: 'rent' , title: 'Aluguel apt', value:302.12},
     { date: new Date(2022, 4 , 23), category: 'food' , title: 'frangos', value:32.12},
     { date: new Date(2022, 4 , 21), category: 'salary' , title: 'Salario trade', value:30000},
-    { date: new Date(2022, 4 , 21), category: 'salary' , title: 'Salario trade', value:30000}
+    { date: new Date(2022, 4 , 21), category: 'salary' , title: 'Salario trade', value:30000},
+    { date: new Date(2022, 5 , 23), category: 'food' , title: 'frangos', value:32.12},
+    { date: new Date(2022, 5 , 21), category: 'salary' , title: 'Salario trade', value:30000},
+    { date: new Date(2022, 5 , 21), category: 'salary' , title: 'Salario trade', value:30000},
+    { date: new Date(2022, 6 , 7), category: 'food' , title: 'Mc Donalds', value:32.12},
+    { date: new Date(2022, 6 , 21), category: 'food' , title: 'Bruger King', value:25},
+    { date: new Date(2022, 6 , 21), category: 'rent' , title: 'Aluguel apt', value:5002.12},
+    { date: new Date(2022, 6 , 23), category: 'food' , title: 'frangos', value:32.12},
+    { date: new Date(2022, 6 , 21), category: 'salary' , title: 'Salario trade', value:30000},
+    { date: new Date(2022, 7 , 7), category: 'food' , title: 'Mc Donalds', value:32.12},
+    { date: new Date(2022, 7 , 21), category: 'food' , title: 'Bruger King', value:25},
+    { date: new Date(2022, 7 , 21), category: 'rent' , title: 'Aluguel apt', value:5002.12},
+    { date: new Date(2022, 7 , 23), category: 'food' , title: 'frangos', value:32.12},
 ];
